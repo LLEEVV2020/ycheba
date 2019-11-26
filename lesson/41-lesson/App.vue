fe0288d06c95ad542a3f88de967a35167424ecc2
LLEEVV2020 <lleevv2020@gmail.com>
2019-11-27 00:15:26 +0400

    add

---
+++ lesson/41-lesson/App.vue
@@ -0,0 +1,40 @@
+<template>
+
+  <div>
+    
+    <h2 v-colored> {{title}} </h2>
+
+    <app-counter></app-counter>
+
+    <app-car-local >
+      <h2 slot="title">{{carName}} + html</h2>
+      <p slot="text">Lorem, ipsum dolor.</p>
+    </app-car-local>
+  </div>
+
+</template>
+
+<script>
+import CarLocal from './CarLocal.vue'
+import Counter from './Counter.vue'
+
+export default {
+  data(){
+    return {
+      carName: 'RFord',
+      title: 'бсмысленный стыд'
+    }
+  },
+  components: {
+    appCarLocal: CarLocal,
+    appCounter: Counter
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  h2{
+    background-color: darkslateblue;
+  }
+
+</style>
