7ee75f2fb2f5407bcff89bab62a91ae1b7a1d625
Лев Милов <milov@ir-tech.ru>
2019-12-02 19:03:04 +0400

    add

---
+++ lesson/81/src/Actions.vue
@@ -0,0 +1,25 @@
+<template>
+    <div >
+        <button class="btn btn-success" @click="updateCounter(1)">Add</button>
+        <button class="btn btn-danger" @click="updateCounter(-1)">Minus</button>
+    </div>
+</template>
+
+<script>
+
+export default {
+    methods: {
+        updateCounter(val){
+            this.$emit('counterUpdated', val)
+        }
+    }
+
+}
+</script>
+
+<style lang="scss" scoped>
+  
+   
+  
+
+</style>
---
+++ lesson/81/src/Counter.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h2>Counter {{counter}}</h2>
+        
+    </div>
+</template>
+
+<script>
+
+export default {
+   props: ['counter']
+}
+</script>
+
+<style lang="scss" scoped>
+  
+  
+</style>
